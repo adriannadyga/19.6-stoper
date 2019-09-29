047b21029fb0acbc88e69fb375e192fd9064d3c1
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-09-29 15:36:07 +0200

    Try make restructure calculate method

--- script.js
+++ script.js
@@ -43,6 +43,18 @@ class Stopwatch {
         this.print();
     }
 
+    /*calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.milisecondsmiliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+    */
     calculate() {
         let {miliseconds, seconds, minutes} = this.times;
         miliseconds += 1;
@@ -55,7 +67,7 @@ class Stopwatch {
             seconds = 0;
         }
     }
-
+    
     stop() {
         this.running = false;
         clearInterval(this.watch);
