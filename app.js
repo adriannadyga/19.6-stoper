bd67ab6f1b79745bb1b9a6a5e5c79d4822f77da4
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-10-01 21:51:55 +0200

    Fix bug

---
+++ app.js
@@ -0,0 +1,12 @@
+class Stopwatch React.Cmponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            times = {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0
+            };
+        }
+    }
+}
--- script.js
+++ script.js
@@ -43,18 +43,18 @@ class Stopwatch {
         this.print();
     }
 
-    /*calculate() {
-        this.times.miliseconds += 1;
-        if (this.times.miliseconds >= 100) {
-            this.times.seconds += 1;
-            this.times.milisecondsmiliseconds = 0;
-        }
-        if (this.times.seconds >= 60) {
-            this.times.minutes += 1;
-            this.times.seconds = 0;
-        }
-    }
-    */
+    // calculate() {
+    //     this.times.miliseconds += 1;
+    //     if (this.times.miliseconds >= 100) {
+    //         this.times.seconds += 1;
+    //         this.times.milisecondsmiliseconds = 0;
+    //     }
+    //     if (this.times.seconds >= 60) {
+    //         this.times.minutes += 1;
+    //         this.times.seconds = 0;
+    //     }
+    // }
+    
     calculate() {
         let {miliseconds, seconds, minutes} = this.times;
         miliseconds += 1;
