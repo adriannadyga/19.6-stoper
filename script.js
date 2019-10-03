c9d371770c3652e8534042576b62f24a432452a9
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-10-03 11:27:29 +0200

    React app

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-//szablon klasy z jednym konstruktorem
-class Stopwatch {
-    constructor(display) { //parametr konstruktora - pole w którym ma być timer
-        this.running = false; //wartość początkowa - czy stoper pracuje
-        this.display = display; //element DOM pod którym znajduje się stoper
-        this.reset(); //metoda resetująca stoper
-        this.print(this.times); //metoda drukująca wynik
+class Stopwatch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            times: {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0
+            }
+        };
+        this.running = false;
+        this.start = this.start.bind(this);
+        this.stop = this.stop.bind(this);
+        this.reset = this.reset.bind(this);
+        this.removeTime = this.removeTime.bind(this);
+        this.saveTime = this.saveTime.bind(this);
     }
 
-    //metody wykonujące się od razu po stworzeniu nowej instancji Stopwatch
-    reset() { //obiekt
-        this.times = {
+    reset() {
+        this.setState ({
+            times: {
             minutes: 0,
             seconds: 0,
             miliseconds: 0
-        };
+            }
+        });
     }
 
-    //ustawia tekst elementu DOM, który jest pod atrybutem display
-    print() { 
-        this.display.innerText = this.format(this.times); //metoda format przygotowuje tekst do wyświetlenia
-    }
-    /*metoda zwraca szablon wykorzystujący obiekt (times) podany do metody; 
-    konstrukcja ${nazwa_zamiennej} umożliwia przekazanie wyniku kolejnej funkcji (pad0) jako jeden z elementów szablonu
-    */
     format(times) {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
     }
@@ -37,29 +40,60 @@ class Stopwatch {
     step() {
         if (!this.running) return;
         this.calculate();
-        this.print();
     }
 
     calculate() {
-        this.times.miliseconds += 1;
-        if (this.times.miliseconds >= 100) {
-            this.times.seconds += 1;
-            this.times.miliseconds = 0;
+        const times = this.state.times;
+        times.miliseconds += 1;
+        if (times.miliseconds >= 100) {
+            times.seconds += 1;
+            times.miliseconds = 0;
         }
-        if (this.times.seconds >= 60) {
-            this.times.minutes += 1;
-            this.times.seconds = 0;
+        if (times.seconds >= 60) {
+            times.minutes += 1;
+            times.seconds = 0;
         }
+        this.setState({times});
     }
 
     stop() {
         this.running = false;
         clearInterval(this.watch);
     }
+
+    saveTime() {
+        let timeItem = document.querySelector(".stopwatch").innerHTML;
+        const node = document.createElement('li');//
+        let textnode = document.createTextNode(timeItem);
+        node.appendChild(textnode);//
+        document.querySelector('.results').appendChild(node);//
+    }
+
+    removeTime() {
+        let times = document.querySelector('.results');
+        times.removeChild(times.lastChild);
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className = "controls">
+                    <a href="#" className = "button" onClick={this.start}>Start</a>
+                    <a href="#" className = "button" onClick={this.stop}>Stop</a>
+                    <a href="#" className = "button" onClick={this.reset}>Reset</a>
+                    <a href="#" className = "button" onClick={this.saveTime}>Save</a>
+                    <a href="#" className = "button" onClick={this.removeTime}>Remove</a>
+                </nav>
+                <div className = "stopwatch">
+                    {this.format(this.state.times)}
+                </div>
+                <ul className = "results">Results:</ul>
+            </div>
+        )
+    }
 };
 
- //funkcja pad) dodaje zero do liczb jednocyfrowych
- function pad0(value) {
+function pad0(value) {
     let result = value.toString();
     if (result.length < 2) {
         result = '0' + result;
@@ -67,11 +101,5 @@ class Stopwatch {
     return result;
 };
 
-const stopwatch = new Stopwatch(document.querySelector('.stopwatch')); 
-
-//metody wykonywane po kliknięciu w przyciski
-let startButton = document.getElementById('start');
-startButton.addEventListener('click', () => stopwatch.start()); 
-
-let stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => stopwatch.stop());
+const app = document.getElementById('root')
+ReactDOM.render(<Stopwatch/>, app);
